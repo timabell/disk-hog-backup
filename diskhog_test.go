06123cfa4fb99be937d5a19259c95229bf34bd22
Tim Abell <tim@timwise.co.uk>
2016-06-19 13:16:53 +0100

    write out a folder to back-up when testing

--- diskhog_test.go
+++ diskhog_test.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestThings(t *testing.T) {
 	x := Pony("rr")
 	assert.Equal(t, x, "rr", "should be intact")
+}
+
+func TestCopyFile(t *testing.T) {
+	source, err := ioutil.TempDir("", "dhb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(source)
+	testFileName := filepath.Join(source, "testfile.txt")
+	contents := []byte("backmeup susie")
+	if err := ioutil.WriteFile(testFileName, contents, 0666); err != nil {
+		log.Fatal(err)
+	}
 }
