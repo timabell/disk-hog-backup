3782bd3ddb91d8b53df6e2130301679768cfc6d5
Tim Abell <t@0x5.uk>
2025-02-20 19:15:36 +0000

    feat: Add symlink support
    Poked the windsurf ai into producing this
    
    fixes https://github.com/timabell/disk-hog-backup/issues/15

--- src/dhcopy/copy_folder.rs
+++ src/dhcopy/copy_folder.rs
@@ -11,7 +11,10 @@ pub fn copy_folder(source: &str, dest: &str) -> io::Result<()> {
 		let path = entry.path();
 		let dest_path = Path::new(dest).join(entry.file_name());
 
-		if path.is_dir() {
+		if path.is_symlink() {
+			let target = fs::read_link(&path)?;
+			std::os::unix::fs::symlink(target, &dest_path)?;
+		} else if path.is_dir() {
 			fs::create_dir_all(&dest_path)?;
 			copy_folder(path.to_str().unwrap(), dest_path.to_str().unwrap())?;
 		} else {
