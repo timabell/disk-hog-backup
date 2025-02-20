15982005fb320ed25d66ca5ee50f78783ee935bf
Tim Abell <t@0x5.uk>
2025-02-20 19:25:32 +0000

    Add windows symlink support
    Build failed on ci
    
        Compiling disk-hog-backup v0.0.0-git
    (D:\a\disk-hog-backup\disk-hog-backup)
    error[E0433]: failed to resolve: could not find `unix` in `os`
      --> src\dhcopy\copy_folder.rs:16:13
       |
    16 |             std::os::unix::fs::symlink(target, &dest_path)?;
       |                      ^^^^ could not find `unix` in `os`

--- src/dhcopy/copy_folder.rs
+++ src/dhcopy/copy_folder.rs
@@ -2,6 +2,12 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
+#[cfg(unix)]
+use std::os::unix::fs::symlink;
+
+#[cfg(windows)]
+use std::os::windows::fs::{symlink_dir, symlink_file};
+
 pub fn copy_folder(source: &str, dest: &str) -> io::Result<()> {
 	println!("backing up folder {} into {}", source, dest);
 	let contents = fs::read_dir(source)?;
@@ -13,7 +19,14 @@ pub fn copy_folder(source: &str, dest: &str) -> io::Result<()> {
 
 		if path.is_symlink() {
 			let target = fs::read_link(&path)?;
-			std::os::unix::fs::symlink(target, &dest_path)?;
+			#[cfg(unix)]
+			symlink(&target, &dest_path)?;
+			#[cfg(windows)]
+			if target.is_dir() {
+				symlink_dir(&target, &dest_path)?;
+			} else {
+				symlink_file(&target, &dest_path)?;
+			}
 		} else if path.is_dir() {
 			fs::create_dir_all(&dest_path)?;
 			copy_folder(path.to_str().unwrap(), dest_path.to_str().unwrap())?;
