590e459dccd63d78eca83d885cf4ec498feaa0a3
Tim Abell <t@0x5.uk>
2025-03-09 00:45:49 +0000

    Remove unused code

--- src/backup_sets/backup_set.rs
+++ src/backup_sets/backup_set.rs
@@ -3,16 +3,6 @@ use chrono::Utc;
 use std::fs;
 use std::path::Path;
 
-pub fn create_empty_set<F>(dest: &str, get_time: F) -> Result<String, std::io::Error>
-where
-	F: Fn() -> chrono::DateTime<Utc>,
-{
-	let set_name = generate_name(get_time);
-	let dir_path = Path::new(dest).join(&set_name);
-	fs::create_dir_all(&dir_path)?;
-	Ok(set_name)
-}
-
 pub fn find_most_recent_set(dest: &str) -> Option<String> {
 	match fs::read_dir(dest) {
 		Ok(entries) => {
