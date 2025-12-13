b146b26b7681109a6c5e5621b53aef6c31f9f40d
Tim Abell <t@0x5.uk>
2025-12-13 13:36:57 +0000

    Add git-cliff to local ci tool setup script

--- ci-tool-setup.sh
+++ ci-tool-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -v
 # Tools needed to run the ci checks locally, only needed when debugging ci failures
 cargo install --locked cargo-deny
+cargo install git-cliff
 pip install --user yamllint
